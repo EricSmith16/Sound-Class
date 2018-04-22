9f1552b4655fdd4ec7f2b897c0394bfff02fe4dc
kilabeez <plzacesteam@hotmail.com>
2018-04-22 23:59:30 +1000

    Add files via upload

---
+++ CSound.cpp
@@ -0,0 +1,127 @@
+#include "CSound.h"
+
+bool CSound::IsValid (int idx, int ent_channel)
+{
+	if (idx == 0)
+		return false;
+
+	if (idx == -1)
+		return false;
+
+	if (ent_channel == 0)
+		return false;
+
+	return true;
+}
+
+bool CSound::IsLocal (int idx)
+{
+	if (idx != _LocalIndex)
+		return false;
+
+	return true;
+}
+
+bool CSound::IsVisible (Vector sound_origin)
+{
+	pmtrace_t tr;
+
+	g_pEngine->pEventAPI->EV_SetTraceHull (2);
+	g_pEngine->pEventAPI->EV_PlayerTrace (_EyePos, sound_origin, PM_WORLD_ONLY, -1, &tr);
+
+	return (bool)(tr.fraction == 1.f);
+}
+
+int CSound::ResolveIndex (int idx, Vector sound_origin)
+{
+	if (ResolveSound.size ())
+	{
+		for (ResolveInfo resolve : ResolveSound)
+		{
+			if (resolve.m_iOrigIndex == idx)
+				return resolve.m_iResolvIndex;
+		}
+	}
+
+	for (int i = 1; i <= g_pEngine->GetMaxClients (); ++i)
+	{
+		cl_entity_s* ent = g_pEngine->GetEntityByIndex (i);
+
+		float fDist = sound_origin.Distance (ent->origin);
+
+		if (fDist < 16.f)
+		{
+			ResolveInfo resolve;
+			resolve.m_iOrigIndex = idx;
+			resolve.m_iResolvIndex = i;
+
+			ResolveSound.push_back (resolve);
+
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+std::deque<SoundInfo>& CSound::GetSound ()
+{
+	return Sound;
+}
+
+void CSound::NewFrame (SoundFrame frame)
+{
+	_LocalIndex = frame.m_iLocalIndex;
+	_EyePos = frame.m_vEyePos;
+}
+
+void CSound::Push (int idx, int ent_channel, char* sound_file, float* origin, SoundFlags flags)
+{
+	if (!IsValid (idx, ent_channel))
+		return;
+
+	if (!(flags & SndFlags_LocalIndex) && IsLocal (idx))
+		return;
+
+	if (flags & SndFlags_OnlyPlayerSounds && !strstr (sound_file, "player"))
+		return;
+
+	Vector sound_origin = origin;
+
+	if (sound_origin.IsZero ())
+		return;
+
+	if (flags & SndFlags_OnlyInvisible && !IsVisible (sound_origin))
+		return;
+
+	if (flags & SndFlags_ResolveIndex && idx > MAX_CLIENTS)
+	{
+		int resolv_idx = ResolveIndex (idx, sound_origin);
+
+		if (resolv_idx != -1 && resolv_idx > 0 && resolv_idx <= MAX_CLIENTS)
+			idx = resolv_idx;
+	}
+
+	SoundInfo sound_info;
+	sound_info.m_vOrigin = sound_origin;
+	sound_info.m_iIndex = idx;
+	sound_info.m_iChannel = ent_channel;
+	sound_info.m_szSoundFile = sound_file;
+	sound_info.m_dwTime = GetTickCount ();
+
+	Sound.push_back (sound_info);
+}
+
+void CSound::Update (DWORD max_valid_time)
+{
+	while (Sound.size () && (GetTickCount () - Sound.front ().m_dwTime) >= max_valid_time)
+		Sound.pop_front ();
+}
+
+void CSound::Clear ()
+{
+	ResolveSound.clear ();
+	Sound.clear ();
+}
+
+std::unique_ptr<CSound> m_pSound = std::make_unique<CSound> ();
---
+++ CSound.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "DLLMain.h"
+
+typedef int SoundFlags;
+
+enum SoundFlags_
+{
+	SndFlags_None = 1 << 1,
+	SndFlags_LocalIndex = 1 << 2,
+	SndFlags_OnlyPlayerSounds = 1 << 3,
+	SndFlags_OnlyInvisible = 1 << 4,
+	SndFlags_ResolveIndex = 1 << 5,
+};
+
+struct SoundFrame
+{
+	Vector		m_vEyePos;
+	int			m_iLocalIndex;
+};
+
+struct SoundInfo
+{
+	Vector		m_vOrigin;
+	DWORD		m_dwTime;
+	char*		m_szSoundFile;
+	int			m_iIndex;
+	int			m_iChannel;
+};
+
+struct ResolveInfo
+{
+	int			m_iOrigIndex;
+	int			m_iResolvIndex;
+};
+
+class CSound
+{
+private:
+	std::deque<SoundInfo> Sound;
+	std::deque<ResolveInfo> ResolveSound;
+
+	Vector		_EyePos;
+	int			_LocalIndex;
+
+	bool		IsValid (int idx, int ent_channel);
+	bool		IsLocal (int idx);
+	bool		IsVisible (Vector sound_origin);
+
+	int			ResolveIndex (int idx,Vector sound_origin);
+
+public:
+	std::deque<SoundInfo>& GetSound ();
+
+	void		NewFrame (SoundFrame frame);
+	void		Push (int idx, int ent_channel, char* sound_file, float* origin, SoundFlags flags = SndFlags_None);
+	void		Update (DWORD max_valid_time = 1000);
+
+	void		Clear ();
+};
+
+extern std::unique_ptr<CSound> m_pSound;
